8db110d2171d4f714a8d0d9e719b38923ecb0893
Antonio <antoniotriolo87@gmail.com>
2019-11-18 00:06:16 +0100

    Add real slider images and background pattern

--- scripts/main.js
+++ scripts/main.js
@@ -22,13 +22,11 @@ function countDown(second,endMinute,endHour,endDay,endMonth,endYear) {
     endDay = typeof(endDay) != 'undefined' ? endDay :  now.getDate();
     endHour = typeof(endHour) != 'undefined' ?  endHour : now.getHours();
     endMinute = typeof(endMinute) != 'undefined' ? endMinute : now.getMinutes();
-//agiungiamo un secondo alla data finale (il nostro taimer mostrera tempo gia dopo 1 secondo.)
     var endDate = new Date(endYear,endMonth,endDay,endHour,endMinute,second+1);
-    var interval = setInterval(function() { //faciamo partire taimer con intervallo di 1 secondo
+    var interval = setInterval(function() {
         var time = endDate.getTime() - now.getTime();
-        if (time < 0) {                      //se la data finale impostata è meno di 1 secondo
-            clearInterval(interval);
-            alert("La data è sbagliata!");
+        if (time < 0) {
+            return;
         } else {
             var days = Math.floor(time / 864e5);
             var hours = Math.floor(time / 36e5) % 24;
@@ -36,12 +34,12 @@ function countDown(second,endMinute,endHour,endDay,endMonth,endYear) {
             var seconds = Math.floor(time / 1e3) % 60;
             var text='</div><div>'
             var end='</div></div><div style="float:left;">,</div>'
-            document.getElementById('mytimer').innerHTML = '<div> </div>'+// qui indichiamo il nome del nostro timer mytimer come esempio //
+            document.getElementById('mytimer').innerHTML = '<div> </div>'+
                 days+text+'giorni'+end+hours+text+'ore'+end+
                 minutes+'minuti'+text+end+seconds+text+'secondi' + '<div></div>';
         }
-        now.setSeconds(now.getSeconds() + 1); //aumentiamo il tempo corrente per 1 secondo
+        now.setSeconds(now.getSeconds() + 1);
     }, 1000);
 }
-countDown(0,0,15,12,6,2020); //impostiamo timer per ora e la data di scadenza, nel mio esempio per 5 maggio 2019 ore 6 minute 30 e secondi 3   //   </script>
+countDown(0,0,15,12,6,2020);
 
